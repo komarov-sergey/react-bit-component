1e9b5d994814a26a0045ef0e1f045ae55cc561f6
Sergey <komarovs33@mail.ru>
2020-12-18 16:14:31 +0300

    p

--- exporter/.bit/components/compilers/react-typescript/bit.envs/3.1.63/dist/react-typescript/src/generate-types.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function generateTypes(_dir: string): Promise<any[]>;
-export declare function getGenericStyle(): string;
-export declare function getSVGType(): string;
-export declare function getImageType(): string;
